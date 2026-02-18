46129cd564edd2be412ccbf889e850b94f0c79fc
Jacky Lee <JackyLeeHongJian@Gmail.com>
2026-02-18 10:40:59 +0800

    Avoid overriding the date variable.

--- weather/start.bat
+++ weather/start.bat
@@ -1,26 +1,25 @@
 echo off
-set d=%date%
-set date=%d:~6,4%-%d:~3,2%-%d:~0,2%
-set t=%time: =0%
-set datetime=%date%-%t:~0,2%-%t:~3,2%
-echo %date% %time% Script started
-echo %date% %time% node index.js
+set YYYYMMDD=%date:~6,4%-%date:~3,2%-%date:~0,2%
+set time0=%time: =0%
+set datetime=%YYYYMMDD%-%time0:~0,2%-%time0:~3,2%
+echo %YYYYMMDD% %time% Script started
+echo %YYYYMMDD% %time% node index.js
 node index.js
 set PUPPETEER_EXECUTABLE_PATH=C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe
-echo %date% %time% cd weather
+echo %YYYYMMDD% %time% cd weather
 cd weather
-echo %date% %time% node index.js city
+echo %YYYYMMDD% %time% node index.js city
 node index.js city
 if %ERRORLEVEL% == 0 (
-echo %date% %time% copy city\forecast.json city\forecast\%datetime%.json
+echo %YYYYMMDD% %time% copy city\forecast.json city\forecast\%datetime%.json
 copy city\forecast.json city\forecast\%datetime%.json
 )
-echo %date% %time% node index.js county
+echo %YYYYMMDD% %time% node index.js county
 node index.js county
 if %ERRORLEVEL% == 0 (
-echo %date% %time% copy county\forecast.json county\forecast\%datetime%.json
+echo %YYYYMMDD% %time% copy county\forecast.json county\forecast\%datetime%.json
 copy county\forecast.json county\forecast\%datetime%.json
 )
-echo %date% %time% cd ..
+echo %YYYYMMDD% %time% cd ..
 cd ..
-echo %date% %time% Script completed
+echo %YYYYMMDD% %time% Script completed
