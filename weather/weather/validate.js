65878fdf3871bdd87c154d7dcc366ef0fc4c9597
Jacky Lee <JackyLeeHongJian@Gmail.com>
2026-04-23 17:56:34 +0800

    Assert city.length

--- weather/weather/validate.js
+++ weather/weather/validate.js
@@ -3,9 +3,11 @@ import fs from 'fs/promises';
 const conditions = {
 	city: {
 		forecastArrLength: { min: 356 },
+		cityLength: { min: 2, max: 5 },
 	},
 	county: {
 		forecastArrLength: { min: 1584 },
+		cityLength: { min: 4, max: 6 },
 	},
 };
 const cityDir = 'city';
@@ -20,6 +22,11 @@ for (const file of files) {
 	const fileHH = fileParts[3];
 	const forecastArr = await fs.readFile(`${dir}/${file}`).then(JSON.parse);
 	console.assert(forecastArr.length >= condition.forecastArrLength.min, file, forecastArr.length);
+	forecastArr.forEach(f => {
+		const { city } = f;
+		console.assert(city.length >= condition.cityLength.min, file, city, city.length);
+		console.assert(city.length <= condition.cityLength.max, file, city, city.length);
+	});
 	const { forecast } = forecastArr[forecastArr.length - 1]; // Validate the last city.
 	console.assert(forecast.length === 7);
 	const day0 = forecast[0]; // Validate the first day.
