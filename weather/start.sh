d7205c420aea7c66e9dcecef7feacb5f9e7d54e3
Jacky Lee <JackyLeeHongJian@Gmail.com>
2026-05-09 21:00:51 +0800

    Change PUPPETEER_EXECUTABLE_PATH from chromium to google-chrome

--- weather/start.sh
+++ weather/start.sh
@@ -3,7 +3,7 @@ echo "$(date +"%F %T.%N") Script started"
 datetime=$(date +"%F-%H-%M")
 echo "$(date +"%F %T.%N") node index.js"
 node index.js
-export PUPPETEER_EXECUTABLE_PATH=/usr/bin/chromium
+export PUPPETEER_EXECUTABLE_PATH=/usr/bin/google-chrome
 echo "$(date +"%F %T.%N") cd weather"
 cd weather
 echo "$(date +"%F %T.%N") node index.js city"
